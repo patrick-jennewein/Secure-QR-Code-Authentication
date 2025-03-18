5d5ce2d4e8534abd1e41425f338df78f963aeacb
Patrick Jennewein <jenneweinp@gmail.com>
2025-03-18 07:19:22 -0500

    Created a bunch of fake data and cleaned up code to have everything in main (for now)

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import cv2
-from webcam import set_webcam_index
-from database import initialize_database
 import sqlite3
+
+
+def set_webcam_index(index):
+    DEFAULT_WEB_CAM = 0
+    EXTERNAL_WEB_CAM = 1
+    if index == 0:
+        return DEFAULT_WEB_CAM
+    elif index == 1:
+        return EXTERNAL_WEB_CAM
+
+
+def initialize_database(conn, cursor):
+    # check if table already exists
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='qr_data'")
+    table_exists = cursor.fetchone()
+
+    # if table does not exist
+    if not table_exists:
+        cursor.execute('''
+            CREATE TABLE qr_data (
+                student_id TEXT PRIMARY KEY,
+                name TEXT,
+                class TEXT,
+                qr_code TEXT UNIQUE,
+                last_scan_time TEXT DEFAULT NULL
+            )
+        ''')
+        print("Table created!")
+
+    conn.commit()
+
 
 def main(webcam_index):
     # open connection to webcam and ensure opened successfully
@@ -11,9 +40,9 @@ def main(webcam_index):
         exit()
 
     # initialize database
-    initialize_database()
-    print("Database initialized!")
-
+    conn = sqlite3.connect("students.db")
+    cursor = conn.cursor()
+    initialize_database(conn, cursor)
 
     # capture webcam by frame
     while True:
@@ -29,6 +58,8 @@ def main(webcam_index):
     # end feed
     webcam.release()
     cv2.destroyAllWindows()
+    conn.close()
+
 
 if __name__ == '__main__':
     webcam_index = set_webcam_index(1)
--- webcam.py
+++
@@ -1,7 +0,0 @@
-def set_webcam_index(index):
-    DEFAULT_WEB_CAM = 0
-    EXTERNAL_WEB_CAM = 1
-    if index == 0:
-        return DEFAULT_WEB_CAM
-    elif index == 1:
-        return EXTERNAL_WEB_CAM
