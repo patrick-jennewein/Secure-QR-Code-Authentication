d999c562b93dbdd5114107ecb6f5c461b7b3991c
Patrick Jennewein <jenneweinp@gmail.com>
2025-03-18 06:45:22 -0500

    Set up the database

---
+++ database.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+def initialize_database():
+    # connect to database
+    conn = sqlite3.connect("students.db")
+    cursor = conn.cursor()
+
+    # create the table if it doesn't exist
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS qr_data (
+            student_id TEXT PRIMARY KEY,
+            name TEXT,
+            class TEXT,
+            qr_code TEXT UNIQUE,
+            last_scan_time TEXT DEFAULT NULL
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
